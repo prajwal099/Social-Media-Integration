b39da59b79ba65a57604eda99eaa0f9f0d80ce61
ASUS <taywadeprajwal00@gmail.com>
2021-05-20 00:06:51 +0530

    SocialMedia Integration

---
+++ app/src/main/java/com/example/socialapp/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.socialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView name;
+    String user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        user=getIntent().getStringExtra("username");
+        name=(TextView)findViewById(R.id.nametextView);
+        name.setText(user);
+    }
+}
---
+++ app/src/main/java/com/example/socialapp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.socialapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    TwitterLoginButton loginButton;
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;;
+    private static final int SIGN_IN = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        googleApiClient=new GoogleApiClient.Builder(this).enableAutoManage(this ,this)
+        .addApi(Auth.GOOGLE_SIGN_IN_API , gso).build();
+
+        signInButton=findViewById(R.id.google_sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent , SIGN_IN);
+            }
+        });
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+
+                loginMethod(session);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+                Toast.makeText(getApplicationContext(),"Login fail",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    public void loginMethod(TwitterSession twitterSession){
+        String userName=twitterSession.getUserName();
+        Intent intent= new Intent(MainActivity.this,HomeActivity.class);
+        intent.putExtra("username",userName);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==SIGN_IN){
+            GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess()){
+
+                startActivity(new Intent(MainActivity.this , ProfileActivity.class));
+                finish();
+            }
+            else {
+                Toast.makeText(this, "Login Has Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
